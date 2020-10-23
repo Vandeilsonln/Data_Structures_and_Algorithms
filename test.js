4a5d1a2c65a06644242f47cd80254381f1eae849
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-22 23:28:01 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+function charCount(str){
+    // make obj ot return at end
+    // loop over string, for each character...
+        // if the char is a number/letter AND is a key in obj, add one to count
+        // if the char is a number/letter AND is not in obj, add it and set value to 1
+    // if char is anything, do nothing
+    // return object at end
+}
