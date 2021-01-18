4ed4d314ae1f876157d805861a49bab7f29ab9fc
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-17 22:51:55 -0300

    anagram 2

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -2,32 +2,30 @@
 // same([1, 2, 3], [1, 9]) -- false
 // same([1, 2, 1], [4, 1, 4]) -- false (must be same frequency)
 
-function same(arr1, arr2) {
-    if (arr1.lenght !== arr2.lenght)
+function validAnagram(first, second) {
+    if (first.length !== second.length)
     {
         return false
     }
 
-    let frequencyCounter1 = {}
-    let frequencyCounter2 = {}
-    for (let val of arr1) {
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    const lookup = {}
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i]
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
     }
 
-    for (let val of arr2) {
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) +1
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i]
+        if (!lookup[letter]){
+            return false
+        } else {
+            lookup[letter -= 1]
+        }
     }
 
-    for (let key in frequencyCounter1) {
-        if (!(key in frequencyCounter2)) {
-            return false
-        }
-        if (frequencyCounter2[key] !== frequencyCounter1[key]) {
-            return false
-        }
-    }
 
     return true
 }
 
-console.log(same('qwerty', 'ytrewq'))
+console.log(validAnagram('qwerty', 'qwerty'))
