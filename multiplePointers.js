596944b23a07b142a28d81502adbc235d929697c
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-18 00:12:29 -0300

    Update multiplePointers.js
    teacher's way to solve this exercise

--- multiplePointers.js
+++ multiplePointers.js
@@ -1,19 +1,13 @@
 function countUniqueValues(arr) {
     let left = 0;
-    let right = 1;
-    let total = 0;
 
-    while(right <= arr.length) {
-        if (arr[left] === arr[right]) {
-            right++
-            continue
-        } else {
-            total++
-            left = right
-            right++
+    for(var right = 1; right <= arr.length; right++) {
+        if (arr[left] !== arr[right]) {
+            left++
+            arr[left] = arr[right]
         }
     }
-    return total
+    return left
 }
 
-console.log(countUniqueValues([-3, -2, -1, 0, 6, 1, 1,  2, 8, 4]))
+console.log(countUniqueValues([1, 2, 2, 3, 4, 4, 5]))
