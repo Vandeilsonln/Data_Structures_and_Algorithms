a5826dd7b2f216c2ef6cc258346baa737d562b28
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-19 23:33:29 -0300

    possible solution

--- optional_challenges/averagePair.js
+++ optional_challenges/averagePair.js
@@ -2,21 +2,19 @@
 // ([-1,0,3,4,5,6], 4.1) - false
 // ([], 4) - false
 
-function averagePair(arr, avg){
-    let first = 0
-    let second = 1
-    let tempSum = arr[0] + arr[1]
+function averagePair(arr, num){
+    let start = 0
+    let end = arr.length - 1
 
-    if(arr.length < 2) return false
-
-    for (let i = 0; i < arr.length - 2; i++){
-        tempSum = tempSum - arr[i] + arr[i+2]
-        if (tempSum/2 === avg)
-        {
-            return true
-        }
-    }
+    while(start  < end)
+    {
+        let avg = (arr[start]+ arr[end])/2
+        if(avg === num) return true
+        else if (avg < num) start++
+        else end--
+    }    
+    return false
 
 }
 
-averagePair([1, 20, 3, 40, 5], 2)
+console.log(averagePair([1, 20, 3, 40, 5], 5))
