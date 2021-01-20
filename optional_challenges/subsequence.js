3a7901f1a124038e3f31b4e065886ac868d829ab
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-19 23:41:46 -0300

    iterative

--- optional_challenges/subsequence.js
+++ optional_challenges/subsequence.js
@@ -1,6 +1,14 @@
 function isSubsequence(sub, full){
-    let result = full.includes(sub)
-    return result
+    var i = 0
+    var j = 0
+
+    if(!sub) return true
+    while(j < full.length) {
+        if (full[j] === sub[i]) i++
+        if (i === sub.length) return true
+        j++
+    }
+    return false
 }
 
 console.log(isSubsequence('hello', 'hello world'))
