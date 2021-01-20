4612a0c640122a4c61bc18e85830bc47211730e9
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-19 22:02:48 -0300

    Update fc_mp.js

--- optional_challenges/fc_mp.js
+++ optional_challenges/fc_mp.js
@@ -1,8 +1,14 @@
+// (1, 2, 3) - false
+// (1, 2, 2) - true
+
 function areThereDuplicates(...numbers){
     let data = {}
 
     for (value of numbers){
-        continue
+        if (data[value] === 1) return true
+        data[value] ? data[value] += 1 : data[value] = 1
     }
+    return false
 }
 
+console.log(areThereDuplicates(1, 2, 3, 4, 5, 1))
