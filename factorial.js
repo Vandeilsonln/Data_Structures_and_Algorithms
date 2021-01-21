871fb6636938a92929739d11afa80db0862074fa
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-21 15:49:06 -0300

    Update factorial.js
    recursively

--- factorial.js
+++ factorial.js
@@ -1,10 +1,6 @@
 function factorial(n){
-    let total = 1
-    for (let i = n; i > 1; i--)
-    {
-        total *= i
-    }
-    return total
+    if (n === 1) return 1
+    return n * factorial(n-1)
 }
 
-console.log(factorial(10))
+console.log(factorial(5))
