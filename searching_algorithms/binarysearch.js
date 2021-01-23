2a6ffb2fb4e571bfa28af32a50a2d5385a874a6f
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-23 00:22:56 -0300

    Update binarysearch.js
    better version, longer way

--- searching_algorithms/binarysearch.js
+++ searching_algorithms/binarysearch.js
@@ -1,20 +1,18 @@
 function binarySearch(sortedArr, num){
     let left = 0
     let right = sortedArr.length - 1
+    var middle = Math.floor((left + right)/2)
 
-    while (left < right){
-        let middle = Math.round((left + right) / 2)
-        if (num === sortedArr[middle]) return sortedArr.indexOf(num)
-    
-    else if (num > sortedArr[middle]) {
-        left = middle
+    while(sortedArr[middle] !== num && left <= end){
+        if(num < sortedArr[middle]){
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+        middle = Math.floor((left + right)/2)
     }
-    else if (num < sortedArr[middle]) {
-        right = middle
-    }
-    
-    }
-    return -1
+    if(sortedArr[middle] === num) return middle
+    else return -1
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], 5))
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], 105))
