c298a6b73bd5a7ff7b489c69168d58d910766de9
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-23 22:49:05 -0300

    noSwaps logics included
    Optimization - it may come in handy if the array is nearly sorted. It prevents the program from keeping looping doing no swaps.

--- searching_algorithms/Bubble_Sort.js
+++ searching_algorithms/Bubble_Sort.js
@@ -3,12 +3,16 @@ function bubbleSort(arr){
         [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
     }
 
+    var noSwaps
     for (let i = arr.length; i > 0; i--){
+        noSwaps = true
         for (let j = 0; j < i-1; j++){
             if (arr[j] > arr[j + 1]){
                 swap(arr, j, j + 1)
+                noSwaps = false
             }
         }
+        if (noSwaps) break
     }
     return arr
 }
