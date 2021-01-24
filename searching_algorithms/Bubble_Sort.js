c5d27f9a276051d8c3953ac8ea90848a0060c540
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-23 22:38:47 -0300

    Create Bubble_Sort.js
    simpler way

---
+++ searching_algorithms/Bubble_Sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr){
+    const swap = (arr, index1, index2) => {
+        [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
+    }
+
+    for (let i = arr.length; i > 0; i--){
+        for (let j = 0; j < i-1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(arr, j, j + 1)
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([1,2,5,4,9,25,98,57,14,99,35, 2]))
