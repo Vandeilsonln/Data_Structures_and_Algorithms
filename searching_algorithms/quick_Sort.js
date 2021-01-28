a21064d80c626b14fe0a7193b41f0b6e569aa566
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-27 21:41:27 -0300

    Update quick_Sort.js

--- searching_algorithms/quick_Sort.js
+++ searching_algorithms/quick_Sort.js
@@ -1,4 +1,4 @@
-function pivot(arr, start=0, end=arr.length-1){
+function pivot(arr, start=0){
     function swap(elements, i, j){
         var temp = elements[i]
         elements[i] = elements[j]
@@ -17,4 +17,20 @@ function pivot(arr, start=0, end=arr.length-1){
     return swapIdx
 }
 
-console.log(pivot([9,4,8,2,1,5,7,6,3]))
+function quickSort(arr, left=0, right=arr.length-1){
+    if (left < right){
+    let pivotIndex = pivot(arr, left, right)
+    quickSort(arr, left, right=pivotIndex-1)    // left
+    quickSort(arr, left=pivotIndex+1, right)
+    }
+    
+    return arr
+}
+
+let num = 1000000
+var bigList = []
+for (let i=0; i<num; i++){
+    bigList.push(Math.random())
+}
+
+console.log(quickSort(bigList))
