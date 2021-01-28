3e1982dd9d4a8637c9ac374ff1d58008c54c35f5
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-28 20:11:29 -0300

    Create singly_linked_lists.js
    create class and push method

---
+++ data_structures/singly_linked_lists.js
@@ -0,0 +1,30 @@
+// piece of data - val
+// reference to next node - next
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(val){
+        var newNode = new Node(val)
+        if(this.head === null){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+}
